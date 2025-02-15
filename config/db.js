f5747fa88c8f12704defb2f20af638032338b468
AnaBMo <anab_mj@outlook.com>
2025-02-15 22:14:07 +0100

    15/02/2025

--- config/db.js
+++ config/db.js
@@ -1,1 +1,17 @@
-//! Configuración y conexión a la base de datos.
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const dbConnection = async() => {
+    try {
+        console.log(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Base de datos conectada con éxito ✅');
+    } catch (error) {
+        console.error(error);
+        throw new Error('Error a la hora de iniciar la base de datos 🚫');
+    }
+};
+
+module.exports = {
+    dbConnection,
+};
--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-//! Servidor, conexión a la base de datos y resto de la aplicación.
+const express = require('express');
+const app = express();
+
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+
+const { dbConnection } = require('./config/db');
+//!const routes = require('./routes');
 
 
-/*
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-*/
+//!app.use('/', routes);
+
+
+dbConnection();
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
